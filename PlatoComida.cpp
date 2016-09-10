bcfd19655c056e3cb599b0cb10b443c000d02c01
FedeGB <fedeagb@gmail.com>
2016-09-10 16:39:43 -0300

    Agregadas clases con funcionalidad base de "Platos de Comida".

---
+++ PlatoComida.cpp
@@ -0,0 +1,39 @@
+//
+// Created by fedenote on 9/10/16.
+//
+
+#include "PlatoComida.h"
+
+PlatoComida::PlatoComida() {
+    this->nombre = "";
+    this->precio = 0;
+    this->status = PlatoComida::NUEVOPEDIDO;
+}
+
+PlatoComida::PlatoComida(std::string name, float price) {
+    this->nombre = name;
+    this->precio = price;
+    this->status = PlatoComida::NUEVOPEDIDO;
+}
+float PlatoComida::getPrecio() {
+    return this->precio;
+}
+
+std::string PlatoComida::getNombre() {
+    return this->nombre;
+}
+int PlatoComida::getStatus() {
+    return this->status;
+}
+
+void PlatoComida::changeStatus(int newStatus) {
+    if(newStatus < this->status) {
+        throw "Cambio de estado de " +
+              std::to_string(this->status) + " a " +
+              std::to_string(newStatus) + " es invalido.";
+    }
+    this->status = newStatus;
+}
+
+PlatoComida::~PlatoComida() {
+}
---
+++ PlatoComida.h
@@ -0,0 +1,33 @@
+//
+// Created by fedenote on 9/10/16.
+//
+
+#ifndef TP1_PLATOCOMIDA_H
+#define TP1_PLATOCOMIDA_H
+
+#include <string>
+
+class PlatoComida {
+private:
+    std::string nombre;
+    float precio;
+    int status;
+
+public:
+    static const int NUEVOPEDIDO = 0;
+    static const int COCINANDO = 1;
+    static const int ENTREGADO = 2;
+    static const int COMIDO = 3;
+
+    PlatoComida();
+    PlatoComida(std::string, float);
+    ~PlatoComida();
+    float getPrecio();
+    std::string getNombre();
+    int getStatus();
+    void changeStatus(int);
+
+};
+
+
+#endif //TP1_PLATOCOMIDA_H
