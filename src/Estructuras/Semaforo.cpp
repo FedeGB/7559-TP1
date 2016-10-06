fc44319ca6258a50f66b0597dfcfd2071250f9f2
horacio <hmk142@hotmail.com>
2016-10-06 13:31:32 -0300

    vuelvo a poner SEM_UNDO

--- src/Estructuras/Semaforo.cpp
+++ src/Estructuras/Semaforo.cpp
@@ -30,7 +30,7 @@ int Semaforo :: p () const {
 
 	operacion.sem_num = 0;	// numero de semaforo
 	operacion.sem_op  = -1;	// restar 1 al semaforo
-	//operacion.sem_flg = SEM_UNDO;
+	operacion.sem_flg = SEM_UNDO;
 
 	int resultado = semop ( this->id,&operacion,1 );
 	return resultado;
@@ -42,7 +42,7 @@ int Semaforo :: v () const {
 
 	operacion.sem_num = 0;	// numero de semaforo
 	operacion.sem_op  = 1;	// sumar 1 al semaforo
-	//operacion.sem_flg = SEM_UNDO;
+	operacion.sem_flg = SEM_UNDO;
 
 	int resultado = semop ( this->id,&operacion,1 );
 	return resultado;
